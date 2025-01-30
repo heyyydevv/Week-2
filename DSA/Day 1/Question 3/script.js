079ea4045f7c1e7d53dd47d49dae8ecd10a52763
Devraj Singh <121109571+heyyydevv@users.noreply.github.com>
2025-01-30 21:29:28 +0530

    Add files via upload

---
+++ DSA/Day 1/Question 3/script.js
@@ -0,0 +1,10 @@
+function isEvenOrOdd(num) {
+    if (number % 2 === 0) {
+           return "Even";
+       } else {
+           return "Odd";
+       }
+   
+   }
+   console.log(isEvenOrOdd(7));  // Output: "Odd"
+   console.log(isEvenOrOdd(8));  // Output: "Even"
