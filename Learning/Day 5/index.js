fbc5b40e279f538381564558b8c32c1f5ef256a3
Devraj Singh <121109571+heyyydevv@users.noreply.github.com>
2025-01-30 20:28:59 +0530

    Add files via upload

---
+++ Learning/Day 5/index.js
@@ -0,0 +1,19 @@
+function nice(world)
+{
+    console.log('Hello, ' + world + '!');
+    console.log('Hello, ' + world + '!');
+    console.log('Hello, ' + world + '!');
+    console.log('Hello, ' + world + '!');
+    console.log('Hello, ' + world + '!');
+
+}
+nice('dev');
+
+
+
+console.log('Hello, world!');
+console.log('Hello, world  11111!');
+console.log('Hello, world  22222!');
+console.log('Hello, world  33333!');
+console.log('Hello, world  44444!');
+console.log('Hello, world  55555!');
