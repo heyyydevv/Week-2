ab7382b48e86c55d6f6cbbbc2e3d1ddc840f5870
Devraj Singh <121109571+heyyydevv@users.noreply.github.com>
2025-01-30 21:24:21 +0530

    2
    Question 2nd

--- DSA/Day 1/script.js
+++ DSA/Day 1/script.js
@@ -1,6 +1,11 @@
-let a = 5, b = 10;
-a = a + b; 
-b = a - b; 
-a = a - b; 
-
-console.log("After swapping: a =", a, ", b =", b);
+function findMax(a, b, c) {
+    if (a >= b && a >= c) {
+          return a;
+      } else if (b >= a && b >= c) {
+          return b;
+      } else {
+          return c;
+      }
+  }
+  
+  console.log(findMax(10, 25, 15)); // Output: 25
