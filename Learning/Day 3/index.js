08f04cb4ae00c928a67c66e2e416eb92c05eef05
Devraj Singh <121109571+heyyydevv@users.noreply.github.com>
2025-01-30 20:25:55 +0530

    Add files via upload

---
+++ Learning/Day 3/index.js
@@ -0,0 +1,18 @@
+console.log('Hello, I am conditional Tutorial!');
+let grace=2;
+
+let age=45;
+
+console.log(age**grace);
+console.log(age+grace);
+console.log(age-grace);
+console.log(age*grace);
+console.log(age/grace);
+if(age+grace>18)
+{
+    console.log('You can drive');
+}
+else
+{
+    console.log('You can not drive');
+}
